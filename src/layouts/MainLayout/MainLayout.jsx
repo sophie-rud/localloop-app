5873b178e9367e7da3802f622275ce8c20e485f8
sophie-rud <sophie.rudio@lapiscine.pro>
2025-11-08 23:07:37 +0100

    fix: minor fixes

--- src/layouts/MainLayout/MainLayout.jsx
+++ src/layouts/MainLayout/MainLayout.jsx
@@ -2,14 +2,12 @@ import Navbar from '../elements/Navbar/Navbar.jsx';
 import Footer from '../elements/Footer/Footer.jsx';
 import Header from '../elements/Header/Header.jsx';
 import { Outlet } from 'react-router-dom'
-import Home from "../../pages/Home/Home.jsx";
 
 function MainLayout() {
     return (
         <>
             <Header/>
             <Outlet/>
-            <Home/>
             <Footer/>
             <Navbar/>
         </>
