f472b256693b08b42edfcddd85069fc84d666466
sophie-rud <sophie.rudio@lapiscine.pro>
2025-12-18 21:31:05 +0100

    refactor(utils): minor improvement on withLoadingAndError

--- src/services/withLoadingAndError.jsx
+++ src/services/withLoadingAndError.jsx
@@ -1,7 +1,7 @@
 async function withLoadingAndError(setState, asyncFunction) {
     setState((state) => ({...state, loading: true, error: null}));
     try {
-        await asyncFunction();
+        return await asyncFunction();
     } catch (err) {
         setState((state) => ({...state, error: err.message || err}));
     } finally {
