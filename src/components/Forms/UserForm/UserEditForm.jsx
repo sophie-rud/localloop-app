3afb2607d7dd91148b042af65f17a337db4b8d48
sophie-rud <sophie.rudio@lapiscine.pro>
2026-01-12 21:47:52 +0100

    refactor(form): apply validators to UserForms

--- src/components/Forms/UserForm/UserEditForm.jsx
+++ src/components/Forms/UserForm/UserEditForm.jsx
@@ -3,10 +3,11 @@ import formClasses from '../Forms.module.css';
 import classes from "./UserEditForm.module.css";
 import Button from "../../ui/Button/Button.jsx";
 import {AuthContext} from "../../../contexts/auth-context.jsx";
+import {hasErrors, validateForm, validators} from "../../../utils/validators.js";
 
 function UserEditForm({ onSubmit, onClose, onDelete }) {
-
     const { user } = useContext(AuthContext);
+    const [errors, setErrors] = useState({});
 
     const [formData, setFormData] = useState({
         username: user?.username || '',
@@ -14,9 +15,34 @@ function UserEditForm({ onSubmit, onClose, onDelete }) {
         avatar: null
     });
 
+    const validationRules = {
+        email: validators.email,
+        username: validators.username,
+        avatar: validators.photo,
+    };
+
     const handleSubmit = async (e) => {
         e.preventDefault();
-        onSubmit(formData);
+
+        setErrors({});
+        const formErrors = validateForm(formData, validationRules);
+        setErrors(formErrors);
+
+        if(hasErrors(formErrors)) {
+            return;
+        }
+
+        const formDataToSubmit = new FormData();
+
+        formDataToSubmit.append('username', formData.username);
+        formDataToSubmit.append('email', formData.email);
+
+        // Add photo if a new one has been selected
+        if (formData.avatar) {
+            formDataToSubmit.append('photo', formData.photo);
+        }
+
+        onSubmit(formDataToSubmit);
     };
 
     const handleInputChange = (field) => (e) => {
@@ -26,6 +52,7 @@ function UserEditForm({ onSubmit, onClose, onDelete }) {
         }));
     };
 
+
     return (
         <form onSubmit={handleSubmit} className={formClasses['form']} >
             <div className={classes['delete-profile-btn']}>
@@ -34,38 +61,47 @@ function UserEditForm({ onSubmit, onClose, onDelete }) {
                 </Button>
             </div>
 
-            <label htmlFor="username">Pseudo</label>
-            <input
-                type="text"
-                id="username"
-                value={formData.username}
-                placeholder="Pseudo"
-                onChange={handleInputChange('username')}
-                className={formClasses['common-input']}
-                required
-            />
+            <div className={formClasses['input-container']}>
+                {errors.username && <p className="error">{errors.username}</p>}
+                <label htmlFor="username">Pseudo</label>
+                <input
+                    type="text"
+                    id="username"
+                    value={formData.username}
+                    placeholder="Pseudo"
+                    onChange={handleInputChange('username')}
+                    className={formClasses['common-input']}
+                    required
+                />
+            </div>
 
-            <label htmlFor="email">Email</label>
-            <input
-                type="email"
-                value={formData.email}
-                placeholder="Email"
-                onChange={handleInputChange('email')}
-                className={formClasses['common-input']}
-                required
-            />
+            <div className={formClasses['input-container']}>
+                {errors.email && <p className="error">{errors.email}</p>}
+                <label htmlFor="email">Email</label>
+                <input
+                    type="email"
+                    value={formData.email}
+                    placeholder="Email"
+                    onChange={handleInputChange('email')}
+                    className={formClasses['common-input']}
+                    required
+                />
+            </div>
 
-            <label htmlFor="avatar">Avatar</label>
-            <input
-                type="file"
-                id="avatar"
-                accept="image/*"
-                className={formClasses['common-file-input']}
-                onChange={(e) => {
-                    const file = e.target.files[0];
-                    setFormData(prev => ({ ...prev, avatar: file }));
-                }}
-            />
+            <div className={formClasses['input-container']}>
+                {errors.photo && <p className="error">{errors.photo}</p>}
+                <label htmlFor="avatar">Avatar</label>
+                <input
+                    type="file"
+                    id="avatar"
+                    accept="image/*"
+                    className={formClasses['common-file-input']}
+                    onChange={(e) => {
+                        const file = e.target.files[0];
+                        setFormData(prev => ({ ...prev, avatar: file }));
+                    }}
+                />
+            </div>
 
             <div>
                 <Button type="submit" className={'small-blue-btn'}>Enregistrer</Button>
--- src/components/Forms/UserForm/UserForm.jsx
+++ src/components/Forms/UserForm/UserForm.jsx
@@ -2,11 +2,12 @@ import { useState, useEffect } from "react";
 import formClasses from '../Forms.module.css';
 import Button from "../../ui/Button/Button.jsx";
 import useUsersStore from "../../../stores/useUsersStore.jsx";
+import {hasErrors, validateForm, validators} from "../../../utils/validators.js";
 
 function UserForm({ onSubmit, onClose }) {
-
     const { selectedUser } = useUsersStore()
 
+    const [errors, setErrors] = useState({});
     const [username, setUsername] = useState("");
     const [email, setEmail] = useState("");
     const [roleId, setRoleId] = useState(1);
@@ -21,6 +22,11 @@ function UserForm({ onSubmit, onClose }) {
         }
     }, [selectedUser]);
 
+    const validationRules = {
+        email: validators.email,
+        username: validators.username,
+    };
+
     const handleSubmit = async (e) => {
         e.preventDefault();
 
@@ -30,6 +36,14 @@ function UserForm({ onSubmit, onClose }) {
             roleId,
             isActive,
         };
+
+        setErrors({});
+        const formErrors = validateForm(userData, validationRules);
+        setErrors(formErrors);
+
+        if(hasErrors(formErrors)) {
+            return;
+        }
 
         onSubmit(userData);
     };
@@ -45,48 +59,58 @@ function UserForm({ onSubmit, onClose }) {
 
     return (
         <form onSubmit={handleSubmit} className={formClasses['form']} >
-            <label htmlFor="username">Pseudo</label>
-            <input
-                type="text"
-                id="username"
-                value={username}
-                placeholder="Pseudo"
-                onChange={handleInputChange(setUsername)}
-                className={formClasses['common-input']}
-                required
-            />
-
-            <label htmlFor="email">Email</label>
-            <input
-                type="email"
-                value={email}
-                placeholder="Email"
-                onChange={handleInputChange(setEmail)}
-                className={formClasses['common-input']}
-                required
-            />
-
-            <label htmlFor="roleId">Rôle</label>
-            <select
-                id="roleId"
-                value={roleId}
-                className={formClasses['common-select-input']}
-                onChange={handleInputChange(setRoleId)}
-            >
-                <option value="1">Utilisateur</option>
-                <option value="2">Admin</option>
-            </select>
-
-            <label>
+            <div className={formClasses['input-container']}>
+                {errors.username && <p className={formClasses['error']}>{errors.username}</p>}
+                <label htmlFor="username">Pseudo</label>
                 <input
-                    type="checkbox"
-                    id="isActive"
-                    className={formClasses['common-checkbox']}
-                    checked={isActive}
-                    onChange={handleInputChange(setIsActive)}
+                    type="text"
+                    id="username"
+                    value={username}
+                    placeholder="Pseudo"
+                    onChange={handleInputChange(setUsername)}
+                    className={formClasses['common-input']}
+                    required
                 />
-                Actif
-            </label>
+            </div>
+
+            <div className={formClasses['input-container']}>
+                {errors.email && <p className={formClasses['error']}>{errors.email}</p>}
+                <label htmlFor="email">Email</label>
+                <input
+                    type="email"
+                    value={email}
+                    placeholder="Email"
+                    onChange={handleInputChange(setEmail)}
+                    className={formClasses['common-input']}
+                    required
+                />
+            </div>
+
+            <div className={formClasses['input-container']}>
+                <label htmlFor="roleId">Rôle</label>
+                <select
+                    id="roleId"
+                    value={roleId}
+                    className={formClasses['common-select-input']}
+                    onChange={handleInputChange(setRoleId)}
+                >
+                    <option value="1">Utilisateur</option>
+                    <option value="2">Admin</option>
+                </select>
+            </div>
+
+            <div className={formClasses['input-container']}>
+                <label>
+                    <input
+                        type="checkbox"
+                        id="isActive"
+                        className={formClasses['common-checkbox']}
+                        checked={isActive}
+                        onChange={handleInputChange(setIsActive)}
+                    />
+                    Actif
+                </label>
+            </div>
 
             <Button type="submit" className={'blue-btn'}>Enregistrer</Button>
             {onClose && <Button type="button" onClick={onClose} className={'green-btn'}>Annuler</Button>}
